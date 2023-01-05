4690b61d4287ea0e422a39a3f8f1df7ee41a1cb7
Doug Hoyte <doug@hcsw.org>
2023-01-05 13:59:47 -0500

    don't link gmp by default

--- rules.mk
+++ rules.mk
@@ -3,7 +3,7 @@ OPT      ?= -O2 -g
 STD      ?= -std=c++20
 CXXFLAGS += $(STD) $(OPT) $(W) -fPIC $(XCXXFLAGS) -DDOCOPT_HEADER_ONLY
 INCS     += -Iinclude -Ibuild -Igolpe/external -Igolpe/external/config/include -Igolpe/external/json/include -Igolpe/external/PEGTL/include -Igolpe/external/hoytech-cpp -Igolpe/external/docopt.cpp -Igolpe/external/loguru -Igolpe/external/quadrable/include
-LDLIBS   += golpe/external/uWebSockets/libuWS.a -ldl -lz -lcrypto -lssl -llmdb -lgmp -lgmpxx -pthread
+LDLIBS   += golpe/external/uWebSockets/libuWS.a -ldl -lz -lcrypto -lssl -llmdb -pthread
 LDFLAGS  += -flto $(XLDFLAGS)
 SRCS    := golpe/logging.cpp build/main.cpp build/config.cpp $(wildcard *.cpp)
 

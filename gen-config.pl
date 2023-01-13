e39dcdc7ed857f8614950de2a99d336eb29af677
Doug Hoyte <doug@hcsw.org>
2023-01-13 16:11:22 -0500

    gen default config

--- gen-config.pl
+++ gen-config.pl
@@ -12,6 +12,8 @@ my $golpe = YAML::LoadFile('./golpe.yaml');
 
 my $config = $golpe->{config} || [];
 
+my $nested = { name => '', items => [], };
+
 foreach my $c (@$config) {
     $c->{nameCpp} = $c->{name};
 
@@ -41,7 +43,38 @@ foreach my $c (@$config) {
     } else {
         die "unknown type: $c->{type}";
     }
+
+    my $n = $nested->{items};
+    my $items = [ split(/__/, $c->{name}) ];
+    my $lastItem = pop @$items;
+
+    for my $k (@$items) {
+        my ($item) = grep { $k eq $_->{name} } @$n;
+        if (!$item) {
+            $item = { name => $k, items => [], };
+            push @$n, $item;
+        }
+        $n = $item->{items};
+    }
+
+    push @$n, {
+        key => $lastItem,
+        %$c,
+    };
 }
+
+
+{
+    open(my $fh, '>', "build/$golpe->{appName}.conf") || die "couldn't write to default config";
+    my $o = '';
+    genDefaultConfig(\$o, $nested, 0);
+    print $fh "##\n";
+    print $fh "## Default $golpe->{appName} config\n";
+    print $fh "##\n\n";
+    print $fh $o;
+}
+
+
 
 my $ctx = {
     config => $config,
@@ -55,3 +88,30 @@ my $tt = Template->new({
 
 $tt->process("golpe/config.h.tt", $ctx, "build/config.h") || die $tt->error(), "\n";
 $tt->process("golpe/config.cpp.tt", $ctx, "build/config.cpp") || die $tt->error(), "\n";
+
+
+
+
+sub genDefaultConfig {
+    my ($o, $n, $indent) = @_;
+
+    my $prefix = "    " x $indent;
+
+    my $first = 1;
+
+    for my $i (@{ $n->{items} }) {
+        $$o .= "\n" if !$first;
+        $first = 0;
+
+        if ($i->{items}) {
+            $$o .= "$prefix$i->{name} {\n";
+            genDefaultConfig($o, $i, $indent + 1);
+            $$o .= "$prefix}\n";
+        } else {
+            $$o .= "$prefix# $i->{desc}\n" if $i->{desc};
+            my $default = $i->{default};
+            $default = qq{"$default"} if $i->{type} eq 'string';
+            $$o .= "$prefix$i->{key} = $default\n";
+        }
+    }
+}

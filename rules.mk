bb00051070eb6e50321b355b42b6d2cbf70c08ca
Thunder-Blaze <darkthunderstormshiva@gmail.com>
2026-08-23 07:24:00 +0530

    feat: update rules.mk for Windows compatibility

--- rules.mk
+++ rules.mk
@@ -5,45 +5,55 @@ W        ?= -Wall
 OPT      ?= -O2 -g
 STD      ?= -std=c++20
 
+ifeq ($(OS),Windows_NT)
+    BIN_EXE = $(BIN).exe
+    WS_LIBS = -lws2_32 -luv -lbcrypt
+    DL_LIBS =
+else
+    BIN_EXE = $(BIN)
+    WS_LIBS =
+    DL_LIBS = -ldl
+endif
+
 CXXFLAGS += $(STD) $(OPT) $(W) -fPIC $(XCXXFLAGS) -DDOCOPT_HEADER_ONLY
 INCS     += -Iinclude -Ibuild -Isrc -Igolpe/external -Igolpe/external/lmdbxx/include -Igolpe/external/config/include -Igolpe/external/json/include -Igolpe/external/PEGTL/include -Igolpe/external/hoytech-cpp -Igolpe/external/docopt.cpp -Igolpe/external/loguru -Igolpe/external/parallel-hashmap -Igolpe/external/session-token-cpp/include
-LDLIBS   += $(shell golpe/if-feature.pl websockets golpe/external/uWebSockets/libuWS.a) $(shell golpe/if-feature.pl db -llmdb) $(shell golpe/if-feature.pl ssl,websockets '-lcrypto -lssl') $(shell golpe/if-feature.pl zlib,websockets -lz) -ldl -pthread
+LDLIBS   += $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl websockets golpe/external/uWebSockets/libuWS.a) $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl db -llmdb) $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl ssl,websockets '-lcrypto -lssl') $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl zlib,websockets -lz) $(DL_LIBS) -pthread $(WS_LIBS)
 LDFLAGS  += -flto $(XLDFLAGS)
-SRCS    := golpe/logging.cpp build/main.cpp $(shell golpe/if-feature.pl config build/config.cpp) $(wildcard src/*.cpp) $(wildcard $(foreach p,$(APPS),src/apps/$(p)/*.cpp))
+SRCS    := golpe/logging.cpp build/main.cpp $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl config build/config.cpp) $(wildcard src/*.cpp) $(wildcard $(foreach p,$(APPS),src/apps/$(p)/*.cpp))
 
 OBJS    := $(SRCS:.cpp=.o)
 DEPS    := $(SRCS:.cpp=.d)
 
-JUNK_ARG := $(shell golpe/pre-build.pl)
+JUNK_ARG := $(shell perl -Igolpe -Igolpe/vendor golpe/pre-build.pl)
 
 SETUP_CHECK_FILE := golpe/external/hoytech-cpp/README.md
 
 .PHONY: all clean setup-golpe update-submodules gitmodules-dev-config
 
-all: $(BIN)
+all: $(BIN_EXE)
 
-$(BIN): $(SETUP_CHECK_FILE) $(OBJS) $(DEPS) $(shell golpe/if-feature.pl db build/defaultDb.h) $(shell golpe/if-feature.pl websockets golpe/external/uWebSockets/libuWS.a)
-	$(CXX) $(OBJS) $(CMDOBJS) $(LDFLAGS) $(LDLIBS) -o $(BIN)
+$(BIN_EXE): $(SETUP_CHECK_FILE) $(OBJS) $(DEPS) $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl db build/defaultDb.h) $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl websockets golpe/external/uWebSockets/libuWS.a)
+	$(CXX) $(OBJS) $(CMDOBJS) $(LDFLAGS) $(LDLIBS) -o $(BIN_EXE)
 
 golpe/external/uWebSockets/libuWS.a:
-	cd golpe/external/uWebSockets && make -j$(NPROC) libuWS.a
+	cd golpe/external/uWebSockets && $(MAKE) -j$(NPROC) libuWS.a
 
-%.o : %.cpp build/golpe.h $(shell golpe/if-feature.pl config build/config.h) $(shell golpe/if-feature.pl db build/defaultDb.h)
+%.o : %.cpp build/golpe.h $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl config build/config.h) $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl db build/defaultDb.h)
 	$(CXX) $(CXXFLAGS) $(INCS) -MMD -MP -MT $@ -MF $*.d -c $< -o $@
 
 build/config.o: OPT=-O0 -g
 
 build/main.cpp: golpe/main.cpp.tt golpe/gen-main.cpp.pl build/app_git_version.h
-	golpe/gen-main.cpp.pl
+	perl -Igolpe -Igolpe/vendor golpe/gen-main.cpp.pl
 
 build/config.cpp: golpe/config.cpp.tt golpe/gen-config.pl $(wildcard golpe.yaml src/apps/*/golpe.yaml)
-	golpe/gen-config.pl
+	perl -Igolpe -Igolpe/vendor golpe/gen-config.pl
 
 build/config.h: build/config.cpp $(wildcard golpe.yaml src/apps/*/golpe.yaml)
 
 build/golpe.h: golpe/golpe.h.tt golpe/gen-golpe.h.pl $(wildcard global.h) $(wildcard *.fbs)
-	golpe/gen-fbs.pl
-	golpe/gen-golpe.h.pl
+	perl -Igolpe -Igolpe/vendor golpe/gen-fbs.pl
+	perl -Igolpe -Igolpe/vendor golpe/gen-golpe.h.pl
 
 -include $(foreach p,$(APPS),src/apps/$(p)/rules.mk)
 
@@ -52,10 +62,10 @@ build/golpe.h: golpe/golpe.h.tt golpe/gen-golpe.h.pl $(wildcard global.h) $(wild
 %.d : ;
 
 build/defaultDb.h: $(wildcard golpe.yaml src/apps/*/golpe.yaml)
-	PERL5LIB=golpe/vendor/ golpe/external/rasgueadb/rasgueadb-generate golpe.yaml build
+	perl -Igolpe/vendor golpe/external/rasgueadb/rasgueadb-generate golpe.yaml build
 
 clean:
-	rm -f $(BIN) src/*.{o,d} src/apps/*/*.{o,d}
+	rm -f $(BIN) $(BIN).exe src/*.{o,d} src/apps/*/*.{o,d}
 	rm -rf build/
 	$(foreach dir, config json lmdbxx PEGTL uWebSockets, $(MAKE) -C golpe/external/$(dir) clean;)
 
@@ -65,12 +75,12 @@ update-submodules:
 
 setup-golpe:
 	cd golpe/external && git submodule update --init hoytech-cpp session-token-cpp docopt.cpp loguru parallel-hashmap \
-	    $(shell golpe/if-feature.pl config config) \
-	    $(shell golpe/if-feature.pl config,json,pegtl 'json PEGTL' ) \
-	    $(shell golpe/if-feature.pl db rasgueadb ) \
-	    $(shell golpe/if-feature.pl db lmdbxx ) \
-	    $(shell golpe/if-feature.pl templar templar ) \
-	    $(shell golpe/if-feature.pl websockets uWebSockets )
+	    $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl config config) \
+	    $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl config,json,pegtl 'json PEGTL' ) \
+	    $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl db rasgueadb ) \
+	    $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl db lmdbxx ) \
+	    $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl templar templar ) \
+	    $(shell perl -Igolpe -Igolpe/vendor golpe/if-feature.pl websockets uWebSockets )
 
 $(SETUP_CHECK_FILE):
 	$(error Please run 'make setup-golpe')
